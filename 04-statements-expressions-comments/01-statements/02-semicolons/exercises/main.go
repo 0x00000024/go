7227620cab9f757a45af4694b2617e7b7cf28397
Ethan Yu <sohbmpethan@gmail.com>
2020-07-13 02:53:05 +0900

    exercises

---
+++ 04-statements-expressions-comments/01-statements/02-semicolons/exercises/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// ---------------------------------------------------------
+// EXERCISE: Shy Semicolons
+//
+//  1. Try to type your statements by separating them using
+//     semicolons
+//
+//  2. Observe how Go fixes them for you
+//
+// ---------------------------------------------------------
+
+func main() {
+	// Uncomment the line of code below; then save the file.
+	//
+	// You will see that Gofmt tool will format your code automatically.
+	// https://golang.org/cmd/gofmt/
+	//
+	// This is because, for Go, it doesn't matter whether
+	// you use semicolons between the statements or not.
+	//
+	// It adds them automatically after all.
+
+	fmt.Println("inanc")
+	fmt.Println("lina")
+	fmt.Println("ebru")
+}
